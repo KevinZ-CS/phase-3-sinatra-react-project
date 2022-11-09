47071cd9b9b8808fbba81203e21a75b28993c0a3
Kevin Zheng <kevzheng27@gmail.com>
2022-11-09 12:28:59 -0500

    Add table and column name to create_workouts migration file

--- db/migrate/20221109172630_create_workouts.rb
+++ db/migrate/20221109172630_create_workouts.rb
@@ -1,4 +1,13 @@
 class CreateWorkouts < ActiveRecord::Migration[6.1]
   def change
+    create_table :workouts do |t|
+      t.string :exercise
+      t.string :weight
+      t.string :sets
+      t.string :reps
+      t.integer :category_id
+      t.boolean :complete
+      t.timestamps
+    
   end
 end
